b6ad23b9f7f42fdbb48e95e8eb933e4e432eafb8
Caio Spagnolo <Caiospagnolo2003@gmail.com>
2024-11-05 11:31:23 -0300

    - leves melhoras rede neural e finalização do dataset

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from sklearn.utils import class_weight
+import numpy as np
 
 datagen = ImageDataGenerator(
     rotation_range=20,
@@ -15,25 +17,24 @@ datagen = ImageDataGenerator(
 )
 
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    'C:\AmbienteDesenvolvimento\ProjetoIA2.1\pythonProject\pollution-sea-dataset\pollution-sea-dataset\datasetGarbage/train',
+    r'C:\caio\pollution-sea-dataset\datasetGarbage/train',
     image_size=(224, 224),
     batch_size=16,
     label_mode='categorical',
-    validation_split=0.2,  # 20% para validação e 80% treinamento
-    subset="training",      # Para o conjunto de treinamento
-    seed=123               # Para reprodutibilidade
+    validation_split=0.2,
+    subset="training",
+    seed=123
 )
 
 validation_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    'C:\AmbienteDesenvolvimento\ProjetoIA2.1\pythonProject\pollution-sea-dataset\pollution-sea-dataset\datasetGarbage/validation',
+    r'C:\caio\pollution-sea-dataset\datasetGarbage/validation',
     image_size=(224, 224),
     batch_size=16,
     label_mode='categorical',
-
 )
 
 test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    'C:\AmbienteDesenvolvimento\ProjetoIA2.1\pythonProject\pollution-sea-dataset\pollution-sea-dataset\datasetGarbage/test',
+    r'C:\caio\pollution-sea-dataset\datasetGarbage/test',
     image_size=(224, 224),
     batch_size=16,
     label_mode='categorical',
@@ -47,18 +48,23 @@ base_model.trainable = False
 model = models.Sequential([
     base_model,
     layers.GlobalAveragePooling2D(),
+    layers.BatchNormalization(),
     layers.Dropout(0.5),  #  uma camada de Dropout
     layers.Dense(512, activation='relu'),
-    layers.Dense(2, activation='softmax')  # 2 classes / garbage e no garbage
+    layers.Dense(3, activation='softmax')  # 3 classes / garbage e no garbage
 ])
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Calcular os pesos das classes
+train_labels = np.concatenate([y for x, y in train_dataset], axis=0)  # Obter labels do conjunto de treinamento
+class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(train_labels.argmax(axis=1)), y=train_labels.argmax(axis=1))
 
 # Configurar callbacks
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=1e-6)
 
 # Treinar o modelo usando o conjunto de treinamento e validação
-model.fit(train_dataset, validation_data=validation_dataset, epochs=10)
+model.fit(train_dataset, validation_data=validation_dataset, epochs=15)
 
 model.save('main.h5')
--- results.py
+++ results.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 model = tf.keras.models.load_model('main.h5')
 
 # Dicionário para mapear classes a labels
-class_labels = {0: 'Resíduo Sólido', 1: 'Rio/MAR Limpo'}
+class_labels = {0: 'Resíduo Sólido', 1: 'Rio/MAR Limpo', 2: 'Manchas de oleo'}
 
 def classificar_imagem(img_path):
     # Carregar a imagem
@@ -15,21 +15,18 @@ def classificar_imagem(img_path):
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)  # Adicionar uma nova dimensão para o batch
 
-    # Normalizar a imagem
     img_array /= 255.0
 
-    # Fazer a predição
     predicao = model.predict(img_array)
+
+    print(f'Probabilidades: {predicao}')
+
     classe_predita = np.argmax(predicao, axis=1)[0]  # Pega o índice da classe com maior probabilidade
 
-    predicao = model.predict(img_array)
-    print(f'Probabilidades: {predicao}')  # Isso mostrará as probabilidades para cada classe
-
-    # Resultado
     return class_labels[classe_predita]
 
 # Usar a função
-caminho_imagem = 'C:\AmbienteDesenvolvimento\ProjetoIA2.1\imagem test\image_000.jpg'
+caminho_imagem = 'C:\caio\Processamento-de-lixo-no-mar\pollution-sea-dataset\datasetGarbage/test/Non Garbage/No_Oil_Spill00996.jpg'
 
 resultado = classificar_imagem(caminho_imagem)
 print(f'A imagem classificada é: {resultado}')
